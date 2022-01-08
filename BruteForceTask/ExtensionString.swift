3b650fc5a226644d1e6a23261738445039a57b6b
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-01-08 20:42:07 +0500

    Add changes in file extension string().

--- BruteForceTask/ExtensionString.swift
+++ BruteForceTask/ExtensionString.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+extension String {
+    
+    var digits: String { return "0123456789" }
+    var lowercase: String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase: String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters: String { return lowercase + uppercase }
+    var printable: String { return digits + letters + punctuation }
+    
+    mutating func replace(at index: Int, with character: Character) {
+        var stringArray = Array(self)
+        stringArray[index] = character
+        self = String(stringArray)
+    }
+    
+    static func random(length: Int = 3) -> String {
+        let base = "abcdefghijklmnopqrstuvw<=>?@[\\]^_`MNOPQRSTUVWXYZ0123456789"
+        var randomString: String = ""
+        
+        for _ in 0..<length {
+            let randomValue = arc4random_uniform(UInt32(base.count))
+            randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+        }
+        
+        return randomString
+    }
+}
