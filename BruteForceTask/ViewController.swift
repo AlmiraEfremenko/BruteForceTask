123e2b068f82287c3d21db3fde3ed81d28e765ed
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-01-12 22:37:02 +0500

    Add changes in method brute force password().

--- BruteForceTask/ViewController.swift
+++ BruteForceTask/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     
     // MARK: - IBOutlets
     
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var buttonColorView: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var buttonOfGenerationPassword: UIButton!
@@ -39,25 +39,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonOfGenerationPassword(_ sender: Any) {
-        self.textField.text = String.random()
+        setupViewBruteForcePassword()
+        let passwordBruteForce = textField.text ?? " "
+        let passwordSplit = passwordBruteForce.split(by: 2)
+        var arrayGenerationPassword = [GenerationPassword]()
         
-        if self.textField.text != "" {
-            textField.isSecureTextEntry = true
-            let passwordSelection = GenerationPassword(password: textField.text ?? "")
-            indicator.startAnimating()
-            buttonOfGenerationPassword.isEnabled = false
-            queue.addOperation(passwordSelection)
-            
-            let operationBlock = BlockOperation {
-                self.label.text = self.textField.text
-                self.textField.isSecureTextEntry = false
-                self.buttonOfGenerationPassword.isEnabled = true
-                self.indicator.stopAnimating()
-                self.indicator.hidesWhenStopped = true
-            }
-            
-            passwordSelection.completionBlock = {
-                self.mainQueue.addOperation(operationBlock)
+        passwordSplit.forEach { i in
+            arrayGenerationPassword.append(GenerationPassword(password: i))
+        }
+        
+        arrayGenerationPassword.forEach { i in
+            queue.addOperation(i)
+        }
+        
+        queue.addBarrierBlock { [unowned self] in
+            OperationQueue.main.addOperation {
+                self.setupViewCompleteBruteForcePassword()
             }
         }
     }
@@ -68,7 +65,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         indicator.hidesWhenStopped = true
         label.text = "Здесь появится ваш пароль"
+        setupButtonGenerationPassword()
+    }
+    
+    // MARK: - Settings
+    
+    private func setupButtonGenerationPassword() {
         buttonOfGenerationPassword.setTitle("Сгенерировать пароль", for: .normal)
         buttonOfGenerationPassword.setTitle("Подбор пароля", for: .disabled)
+    }
+    
+    private func setupViewBruteForcePassword() {
+        textField.text = String.random(length: 10)
+        textField.isSecureTextEntry = true
+        indicator.startAnimating()
+        buttonOfGenerationPassword.isEnabled = false
+    }
+    
+    private func setupViewCompleteBruteForcePassword() {
+        label.text = self.textField.text
+        textField.isSecureTextEntry = false
+        buttonOfGenerationPassword.isEnabled = true
+        indicator.stopAnimating()
+        indicator.hidesWhenStopped = true
     }
 }
